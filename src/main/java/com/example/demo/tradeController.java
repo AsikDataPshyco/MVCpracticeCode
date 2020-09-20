926255d210eb99b1081c86c6e6f7f01564c1b4af
Asik_Ali_ <asketltester@gmail.com>
2020-09-19 19:35:40 -0500

    ModelandView Object
    Create a Model and View method, create a variable and create a model
    view object and set the view and return it

--- src/main/java/com/example/demo/tradeController.java
+++ src/main/java/com/example/demo/tradeController.java
@@ -5,11 +5,14 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 //import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 
-public class tradeController {
+//pass the client data using http session
+/*public class tradeController {
 	@RequestMapping("/trades")
 	//@ResponseBody
 	public String trades(HttpServletRequest req)
@@ -20,6 +23,18 @@ public class tradeController {
 		//System.out.println("Welcome to Trade Review");				
 		return "trades.jsp";
 	
-	}
+	}*/
+//pass the client data without using https
+	public class tradeController {
+		@RequestMapping("/trades")
+		//@ResponseBody
+		public ModelAndView trades(@RequestParam("tname")String tnames)
+		{
+		    ModelAndView mv = new ModelAndView();
+		    mv.addObject("tname",tnames);
+		    mv.setViewName("trades.jsp");							
+			return mv;
+		
+		}
 
 }
